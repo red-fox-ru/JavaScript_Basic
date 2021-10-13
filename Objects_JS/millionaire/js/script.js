4ac52dd0e47f1627487a38799c4b92c0e2cdebcf
red-F0X <stas-panin@mail.ru>
2021-10-13 20:36:01 +0300

    Add func light correct answer

--- Objects_JS/millionaire/js/script.js
+++ Objects_JS/millionaire/js/script.js
@@ -1,4 +1,4 @@
-var step = 0, correctAns = 0, lsNameButton = ['A: ',  'B: ', 'C: ', 'D: '];
+var step = 0, correctAns = 0, lsNameButton = ['A: ',  'B: ', 'C: ', 'D: '], lsId = [];
 
 // этот метод работает только на сервере, на локальном в частности
 function readTextFile(file, callback) {
@@ -15,13 +15,42 @@ function readTextFile(file, callback) {
 
 
 function questionRead(data) {
-    document.getElementById('question_answer').innerHTML = data[step].question;
-    for (var qs = 0; qs < 4; qs++){
-        document.getElementById('ans_'+qs).innerHTML = lsNameButton[qs] + data[step].answer[qs]
+    let lsRandLvl = [];
+    let stepQs = step+1
+    for (let el = 0; el < lsId.length; el++){
+        if (lsId[el].toString().length === 4 && stepQs.toString().length === 1) {
+            if (lsId[el].toString().indexOf(stepQs) === 0) {
+                console.log(lsId[el])
+                lsRandLvl.push(lsId.indexOf(lsId[el]))
+            }
+        }
+        else if (lsId[el].toString().length === 5 && stepQs.toString().length === 2){
+            if (lsId[el].toString().indexOf(stepQs) === 0){
+                console.log(lsId[el])
+                lsRandLvl.push(lsId.indexOf(lsId[el]))
+            }
+        }
     }
-    correctAns = data[step].correct_answer
-    console.log(data[step])
+    let numRand = lsRandLvl[Math.floor(Math.random() * lsRandLvl.length)]
+    document.getElementById('question_answer').innerHTML = data[numRand].question;
+    for (var qs = 0; qs < 4; qs++){
+        document.getElementById('ans_'+qs).innerHTML = lsNameButton[qs] + data[numRand].answer[qs]
+    }
+    correctAns = data[numRand].correct_answer
+    lsRandLvl.length = 0
+}
 
+function viewAns(){
+    let button = document.getElementById('ans_'+correctAns)
+    button.style.fontWeight = "bold";
+    button.style.boxShadow  = "2px 2px 10px 0 rgba(112,54,190, 0.7) inset, 0px 5px 10px 0 rgba(63,81,181, 0.8)";
+    button.style.color = "#2addd8";
+}
+
+function loseAns() {
+    document.getElementById('msg').style.display = 'flex';
+    document.getElementById('msg').innerHTML = '<h1>Вы проиграли!</h1> ' +
+        '<button id="msg_btn" onclick="location.reload()">ЗАКРЫТЬ</button>';
 }
 
 function clickAnswerA(){
@@ -31,8 +60,8 @@ function clickAnswerA(){
         document.getElementById('step'+step).classList.add('total');
         levelGenerate()
     }else {
-        document.getElementById('msg').innerHTML = '<h1>Вы проиграли!</h1> ' +
-            '<button id="msg_btn" onclick="location.reload()">ЗАКРЫТЬ</button>';
+        setTimeout(viewAns, 500);
+        setTimeout(loseAns, 3000);
     }
 }
 
@@ -43,8 +72,8 @@ function clickAnswerB(){
         document.getElementById('step'+step).classList.add('total');
         levelGenerate()
     }else {
-        document.getElementById('msg').innerHTML = '<h1>Вы проиграли!</h1> ' +
-            '<button id="msg_btn" onclick="location.reload()">ЗАКРЫТЬ</button>';
+        setTimeout(viewAns, 500);
+        setTimeout(loseAns, 3000);
     }
 }
 
@@ -55,8 +84,8 @@ function clickAnswerC(){
         document.getElementById('step'+step).classList.add('total');
         levelGenerate()
     }else {
-        document.getElementById('msg').innerHTML = '<h1>Вы проиграли!</h1> ' +
-                    '<button id="msg_btn" onclick="location.reload()">ЗАКРЫТЬ</button>';
+        setTimeout(viewAns, 500);
+        setTimeout(loseAns, 3000);
     }
 }
 
@@ -67,8 +96,8 @@ function clickAnswerD() {
         document.getElementById('step'+step).classList.add('total');
         levelGenerate()
     }else {
-        document.getElementById('msg').innerHTML = '<h1>Вы проиграли!</h1> ' +
-            '<button id="msg_btn" onclick="location.reload()">ЗАКРЫТЬ</button>';
+        setTimeout(viewAns, 500);
+        setTimeout(loseAns, 3000);
     }
 }
 
@@ -84,6 +113,9 @@ function levelGenerate() {
 
 readTextFile("js/questions.json", function(text){
     var data = JSON.parse(text);
+    for (let elData = 0; elData < data.length; elData++) {
+        lsId.push(data[elData].id)
+    }
     questionRead(data)
 });
 
